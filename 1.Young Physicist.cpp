0daff068f7a70ef5b56a431d0718d95e16046eaa
Shruti Jaiswal <81301875+shrutij11@users.noreply.github.com>
2021-12-21 19:26:53 +0530

    Update 1.Young Physicist.cpp

--- 1.Young Physicist.cpp
+++ 1.Young Physicist.cpp
@@ -3,18 +3,24 @@ using namespace std;
  
 int main() {
 	// your code goes here
-	int yr;
-	cin>>yr;
-	while(true){
-	    yr++;
-	    int a = yr/1000;
-	     int b = yr/100 %10;
-	      int c = yr/10 %10;
-	       int d = yr%10;
-	       if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d){
-	           cout<<yr;
-	           break;
-	       }
-	}
+	int t;
  
+	cin >>t;
+	int arr[3] ={0};
+	int a[3]={0};
+	while(t--){
+      for(int i=0; i<3; i++){
+      	cin>>a[i];
+      	a[i] = a[i]+arr[i];
+      	arr[i]= a[i];
+      }
+ 
+	}
+	
+	if (a[0] ==0 && a[1]==0 && a[2]==0)
+ 
+		cout<<"YES";
+	else 
+		cout<<"NO";
+	return 0;
 }
