197327f8cd6b595dad5a9fe9db006db075304ada
Shruti Jaiswal <81301875+shrutij11@users.noreply.github.com>
2021-12-21 19:25:01 +0530

    Create 1.Young Physicist.cpp

---
+++ 1.Young Physicist.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int yr;
+	cin>>yr;
+	while(true){
+	    yr++;
+	    int a = yr/1000;
+	     int b = yr/100 %10;
+	      int c = yr/10 %10;
+	       int d = yr%10;
+	       if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d){
+	           cout<<yr;
+	           break;
+	       }
+	}
+ 
+}
